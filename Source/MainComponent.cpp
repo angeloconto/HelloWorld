8a86c6167c7c515a14abcdc2611a860200b26f3b
Angelo <angeloconto@gmail.com>
2020-03-25 23:21:35 +0100

    testo di nuovo verde

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -25,7 +25,7 @@ void MainComponent::paint (Graphics& g)
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
 
     g.setFont (Font (32.0f));
-    g.setColour (Colours::red);
+    g.setColour (Colours::green);
     g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
 }
 
